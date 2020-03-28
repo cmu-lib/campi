38c5c413abb763473cc570ceaf8b04dff1e88a55
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-03-28 14:12:07 -0400

    paginate photo grid

--- vue/src/components/PhotoGrid.vue
+++ vue/src/components/PhotoGrid.vue
@@ -1,13 +1,18 @@
 <template>
-  <b-row flex>
-    <router-link
-      v-for="image in images"
-      :key="image.id"
-      :to="{name: 'Photo', params: {id: image.id}}"
-    >
-      <b-img lazy :src="image.image.square" blank-width="200" blank-height="200" />
-    </router-link>
-  </b-row>
+  <b-container fluid v-if="!!images">
+    <b-pagination v-model="current_page" :total-rows="images.count" :per-page="100" />
+    <b-row flex>
+      <router-link
+        v-for="image in images.results"
+        :key="image.id"
+        :to="{name: 'Photo', params: {id: image.id}}"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <b-img lazy :src="image.image.square" blank-width="150" blank-height="150" />
+      </router-link>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
@@ -15,18 +20,27 @@ import { HTTP } from "../main";
 export default {
   name: "PhotoGrid",
   data() {
-    return {};
+    return {
+      current_page: 1
+    };
   },
   asyncComputed: {
     images() {
-      return HTTP.get("/photograph/").then(
+      return HTTP.get("/photograph/", {
+        params: { offset: this.rest_page }
+      }).then(
         results => {
-          return results.data.results;
+          return results.data;
         },
         error => {
           console.log(error);
         }
       );
+    }
+  },
+  computed: {
+    rest_page() {
+      return (this.current_page - 1) * 100;
     }
   }
 };
