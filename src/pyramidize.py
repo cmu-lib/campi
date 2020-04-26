0a053b694033df50719709257fca70247fc678b7
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-04-26 12:48:35 -0400

    standardize new filename extensions

--- src/pyramidize.py
+++ src/pyramidize.py
@@ -17,13 +17,27 @@ manifest = [x.strip() for x in manifest]
 concordance = []
 
 for fp in tqdm.tqdm(manifest):
-    init_replace = re.sub(r" #\(\)", "_", fp)
+    init_replace = re.sub(r"[ #\(\)]", "_", fp.encode("ascii", "ignore").decode())
     np_name = "pyramidized/" + re.sub(r"_+", "_", init_replace)
+    np_name_standard = np_name.replace(".TIF", ".tif").replace(".TIFF", ".tif")
     if not os.path.isfile(np_name):
         path_components = os.path.split(np_name)
         os.makedirs(path_components[0], exist_ok=True)
-        call = ["vips", "im_vips2tiff", fp, f"{np_name}:jpeg:75,tile:256x256,pyramid"]
+        call = [
+            "vips",
+            "tiffsave",
+            fp,
+            np_name_standard,
+            "--tile",
+            "--pyramid",
+            "--compression",
+            "jpeg",
+            "--tile-width",
+            "512",
+            "--tile-height",
+            "512",
+        ]
         pyr_res = subprocess.run(call, stdout=subprocess.PIPE)
     concordance.append({"original": fp, "new": np_name})
 
-json.dump(concordance, open("campi_files.json", "wb"))
+json.dump(concordance, open("campi_files.json", "w"))
