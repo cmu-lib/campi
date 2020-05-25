e524899bd0e50a4377284c0858b94fd32966fa86
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-05-25 12:20:36 -0400

    clean up directory spacing

--- vue/src/components/Directory.vue
+++ vue/src/components/Directory.vue
@@ -1,16 +1,16 @@
 <template>
-  <b-list-group-item v-if="!!directory" class="p-1" :active="directory.text_match">
-    <BIconFolderPlus v-if="(directory.children.length > 0) & !expanded" @click="expanded=true" />
-    <BIconFolderMinus
-      v-else-if="(directory.children.length > 0) & expanded"
-      @click="expanded=false"
-    />
-    <BIconBlank v-else />
-    <span
-      class="ml-2"
-      @click="$emit('selected', directory)"
-    >{{ directory.label }} ({{ directory.n_images }})</span>
-    <b-list-group v-if="expanded" class="ml-3">
+  <b-list-group-item v-if="!!directory" :active="directory.text_match" class="my-0 py-1 mr-0 pr-0">
+    <div class="d-flex justify-content-between align-items-center">
+      <BIconFolderPlus v-if="(directory.children.length > 0) & !expanded" @click="expanded=true" />
+      <BIconFolderMinus
+        v-else-if="(directory.children.length > 0) & expanded"
+        @click="expanded=false"
+      />
+      <BIconBlank v-else />
+      <span class="ml-2 text-truncate" @click="$emit('selected', directory)">{{ directory.label }}</span>
+      <b-badge variant="info" class="ml-auto mr-1">{{ directory.n_images }}</b-badge>
+    </div>
+    <b-list-group v-if="expanded" class="ml-3 mr-0 pr-0">
       <Directory
         v-for="child in directory.children"
         :key="child.id"
