c03900c17557d1062bc3f165afcea7005d36388c
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-06-02 14:30:51 -0400

    adjust embeddings args actions to accept strings

--- rest/cv/management/commands/calculate_embeddings.py
+++ rest/cv/management/commands/calculate_embeddings.py
@@ -8,24 +8,23 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "--model",
-            action="store_true",
+            "model",
+            nargs="+",
             help="Name of pytorch model whose embedding method has already been defined in cv.models",
             type=str,
         )
         parser.add_argument(
-            "--label", action="store_true", help="label to add to this model", type=str
+            "--label", action="store", help="label to add to this model"
         )
         parser.add_argument(
-            "--n_dimensions",
-            action="store_true",
+            "--n-dimensions",
+            action="store",
             help="Number of dimensions in the embeddings produced by this model",
-            type=int,
         )
 
     def handle(self, *args, **options):
-        torch_model = getattr(cv.models, args["model"]).objects.get_or_create(
-            label=args["label"], n_dimensions=args["n_dimensions"]
+        torch_model = getattr(cv.models, options["model"][0]).objects.get_or_create(
+            label=options["label"], n_dimensions=int(options["n_dimensions"])
         )[0]
         allpics = photograph.models.Photograph.objects.all()
         torch_model.build_embeddings(allpics)
