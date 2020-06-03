a0793f8e24b4fae12b2b2b8b1c894a3f9ee0586d
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-06-03 17:21:23 -0400

    go back to avenir

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -57,7 +57,7 @@ export default {
 
 <style>
 #app {
-  font-family: "Open Sans", Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
