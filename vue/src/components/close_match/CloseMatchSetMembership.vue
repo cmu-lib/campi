b110b4be28ca4be199eda5ffa05a6e5ed8a7d9c7
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-06-04 16:31:26 -0400

    cmsm exclusive toggling buttons

--- vue/src/components/close_match/CloseMatchSetMembership.vue
+++ vue/src/components/close_match/CloseMatchSetMembership.vue
@@ -1,18 +1,34 @@
 <template>
   <div>
-    <b-row>
+    <b-row align-v="center" align-h="around">
       <b-button-group>
-        <b-button variant="success" :pressed="selection==true">
+        <b-button
+          :variant="accept_variant"
+          size="sm"
+          :pressed="selection==true"
+          @click="selection=true"
+        >
           <BIconCheck2 />
         </b-button>
-        <b-button variant="danger" :pressed="selection">
+        <b-button
+          :variant="reject_variant"
+          size="sm"
+          :pressed="selection==false"
+          @click="selection=false"
+        >
           <BIconX />
         </b-button>
+        <b-button variant="light" size="sm">
+          <BIconStarFill v-if="primary" variant="warning" @click="primary_toggle" />
+          <BIconStar v-else @click="primary_toggle" />
+        </b-button>
       </b-button-group>
-      <BIconStarFill v-if="primary" />
-      <BIconStar v-else />
     </b-row>
-    <b-img :src="close_match_set_membership.photograph.image.square" width="150" />
+    <b-img :src="close_match_set_membership.photograph.image.square" width="150" :id="popover_id" />
+    <b-popover :target="popover_id" triggers="hover" placement="top">
+      <template v-slot:title>{{ popover_title }}</template>
+      <b-img :src="popover_preivew_src" />
+    </b-popover>
   </div>
 </template>
 
@@ -30,6 +46,10 @@ export default {
     close_match_set_membership: {
       type: Object,
       required: true
+    },
+    popup_size: {
+      type: Number,
+      default: 500
     }
   },
   data() {
@@ -37,6 +57,64 @@ export default {
       selection: null,
       primary: false
     };
+  },
+  computed: {
+    accept_variant() {
+      if (this.selection == true) {
+        return "success";
+      } else {
+        return "light";
+      }
+    },
+    reject_variant() {
+      if (this.selection == false) {
+        return "danger";
+      } else {
+        return "light";
+      }
+    },
+    popover_title() {
+      return (
+        this.close_match_set_membership.photograph.id +
+        "-" +
+        this.close_match_set_membership.distance
+      );
+    },
+    popover_id() {
+      return (
+        "image-popover" +
+        this.close_match_set_membership.id +
+        "-" +
+        this.close_match_set_membership.photograph.id
+      );
+    },
+    popover_preivew_src() {
+      return (
+        this.close_match_set_membership.photograph.image.id +
+        "/full/!" +
+        this.popup_size +
+        "," +
+        this.popup_size +
+        "/0/default.jpg"
+      );
+    }
+  },
+  methods: {
+    primary_toggle() {
+      this.primary = !this.primary;
+    }
+  },
+  watch: {
+    selection() {
+      if (this.selection == false) {
+        this.primary = false;
+      }
+    },
+    primary() {
+      if (this.primary) {
+        this.selection = true;
+      }
+    }
   }
 };
 </script>
