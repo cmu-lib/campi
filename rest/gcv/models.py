eaee81bfcbc2fe66481c08543774b92f85145dcc
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-06-12 07:55:11 -0400

    add date created to GCVResponse

--- rest/gcv/models.py
+++ rest/gcv/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.postgres.fields import JSONField
 import photograph
+from campi.models import dateModifiedModel
 
 
-class GCVResponse(models.Model):
+class GCVResponse(dateModifiedModel):
     photograph = models.OneToOneField(
         photograph.models.Photograph,
         on_delete=models.CASCADE,
