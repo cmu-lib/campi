7254afbb32f4b21ec0bbf9bd88e81d1a582081f8
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-07-02 10:53:44 -0400

    photograph filtering tests

--- rest/photograph/tests.py
+++ rest/photograph/tests.py
@@ -69,3 +69,51 @@ class PhotographListView(TestCase):
             self.assertIn(k, res.data)
         self.assertIn("full", res.data["image"])
         self.assertIn("id", res.data["directory"])
+
+    @as_auth()
+    def test_filter_directory(self):
+        dir_id = models.Photograph.objects.first().directory.id
+        res = self.client.get(self.ENDPOINT, data={"directory": dir_id})
+        self.assertEqual(res.status_code, 200)
+        for p in res.data["results"]:
+            self.assertTrue(p["directory"]["id"] == dir_id)
+
+    @as_auth()
+    def test_filter_job(self):
+        job_id = models.Photograph.objects.filter(job__isnull=False).first().job.id
+        res = self.client.get(self.ENDPOINT, data={"job": job_id})
+        self.assertEqual(res.status_code, 200)
+        for p in res.data["results"]:
+            self.assertTrue(p["job"]["id"] == job_id)
+
+    @as_auth()
+    def test_filter_job_tag(self):
+        job_tag = (
+            models.Photograph.objects.filter(job__isnull=False).first().job.tags.first()
+        )
+        res = self.client.get(self.ENDPOINT, data={"job_tag": job_tag.id})
+        self.assertEqual(res.status_code, 200)
+        for p in res.data["results"]:
+            self.assertEqual(job_tag.jobs.first().id, p["job"]["id"])
+
+    @as_auth()
+    def test_filter_image_path(self):
+        pathstring = models.Photograph.objects.first().image_path
+        res = self.client.get(self.ENDPOINT, data={"image_path": pathstring})
+        self.assertEqual(res.status_code, 200)
+        for p in res.data["results"]:
+            self.assertIn(p["filename"], pathstring)
+
+    @as_auth()
+    def test_filter_tag(self):
+        tag = (
+            models.Photograph.objects.filter(photograph_tags__isnull=False)
+            .first()
+            .photograph_tags.first()
+            .tag
+        )
+        res = self.client.get(self.ENDPOINT, data={"tag": tag.id})
+        self.assertEqual(res.status_code, 200)
+        for p in res.data["results"]:
+            self.assertIn(tag.id, [t["tag"]["id"] for t in p["photograph_tags"]])
+
