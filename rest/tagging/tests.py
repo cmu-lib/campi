15d1b70b20ce33253ffe588e8e05f0f34bbc90ee
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2020-07-08 11:22:49 -0400

    starter tests for taggingtask patching

--- rest/tagging/tests.py
+++ rest/tagging/tests.py
@@ -16,11 +16,6 @@ class TagListView(TestCase):
         cls.OBJ1 = models.Tag.objects.first()
         cls.OBJ2 = models.Tag.objects.last()
 
-    @as_auth()
-    def test_get(self):
-        res = self.client.get(self.ENDPOINT)
-        self.assertEqual(res.status_code, 200)
-
     def test_noaccess(self):
         noaccess(self)
 
@@ -88,3 +83,70 @@ class TagListView(TestCase):
                     photograph_tags__tag=tag["id"], job__tags=target_job_tag
                 ).exists()
             )
+
+
+class TaggingTaskListView(TestCase):
+    fixtures = ["campi/fixtures/test.json"]
+
+    ENDPOINT = reverse("taggingtask-list")
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.OBJ1 = models.TaggingTask.objects.first()
+        cls.OBJ2 = models.TaggingTask.objects.last()
+
+    def test_noaccess(self):
+        noaccess(self)
+
+    @as_auth()
+    def test_get(self):
+        res = self.client.get(self.ENDPOINT)
+        self.assertEqual(res.status_code, 200)
+        for k in ["id", "tag", "pytorch_model", "assigned_user"]:
+            self.assertIn(k, res.data["results"][0])
+        self.assertIn("label", res.data["results"][0]["tag"])
+
+    @as_auth()
+    def test_get_detail(self):
+        res = self.client.get(f"{self.ENDPOINT}{self.OBJ1.id}/")
+        self.assertEqual(res.status_code, 200)
+        for k in ["id", "tag", "pytorch_model", "assigned_user"]:
+            self.assertIn(k, res.data)
+        self.assertIn("label", res.data["tag"])
+
+    @as_auth()
+    def test_post(self):
+        bad_res = self.client.post(
+            f"{self.ENDPOINT}",
+            {"tag": self.OBJ1.tag.id, "pytorch_model": self.OBJ1.pytorch_model.id},
+        )
+        self.assertEqual(bad_res.status_code, 400)
+        newtask = models.Tag.objects.filter(tasks__isnull=True).first()
+        res = self.client.post(
+            f"{self.ENDPOINT}",
+            {"tag": newtask.id, "pytorch_model": self.OBJ1.pytorch_model.id},
+        )
+        for k in ["id", "tag", "pytorch_model"]:
+            self.assertIn(k, res.data)
+        new_task_res = self.client.get(f"{self.ENDPOINT}{res.data['id']}/")
+        self.assertEqual("root", new_task_res.data["assigned_user"]["username"])
+
+    @as_auth()
+    def test_patch(self):
+        res = self.client.put(
+            f"{self.ENDPOINT}{self.OBJ1.id}/",
+            {"tag": self.OBJ1.tag.id, "pytorch_model": self.OBJ1.pytorch_model.id},
+        )
+        self.assertEqual(res.status_code, 200)
+        put_res = self.client.get(f"{self.ENDPOINT}{self.OBJ1.id}/")
+        self.assertEqual("root", put_res.data["assigned_user"]["username"])
+
+    @as_auth("bob")
+    def test_new_patch(self):
+        res = self.client.put(
+            f"{self.ENDPOINT}{self.OBJ1.id}/",
+            {"tag": self.OBJ1.tag.id, "pytorch_model": self.OBJ1.pytorch_model.id},
+        )
+        self.assertEqual(res.status_code, 200)
+        put_res = self.client.get(f"{self.ENDPOINT}{self.OBJ1.id}/")
+        self.assertEqual("bob", put_res.data["assigned_user"]["username"])
